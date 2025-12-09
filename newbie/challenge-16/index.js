bc990ede271d9fe65795bb0cdeda965917e5688a
erwan-ds <140492141+erwan-ds@users.noreply.github.com>
2025-12-09 11:31:44 +0100

    Challenge 16 complete

--- newbie/challenge-16/index.js
+++ newbie/challenge-16/index.js
@@ -5,22 +5,18 @@ items.forEach(item => {
 
   header.addEventListener('click', () => {
 
-    // Si déjà ouvert → fermer uniquement cet item
     if (item.classList.contains('open')) {
       item.classList.remove('open');
       return;
     }
-
-    // Fermer tous les autres items, MAIS PAS l'item cliqué
     items.forEach(i => {
       if (i !== item) {
         i.classList.remove('open');
       }
     });
 
-    // Attendre un peu puis ouvrir celui-ci
     setTimeout(() => {
       item.classList.add('open');
-    }, 210); // délai d’animation
+    }, 210); 
   });
 });
